a5b77172d2e337e2fecf6d8c23b47bd8a5da8805
surajpa123 <pathaksuraj136@gmail.com>
2024-01-12 17:25:48 +0530

    added

--- backEnd/index.js
+++ backEnd/index.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cors({ origin: '*' }));
 
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://mern-assignment-pink.vercel.app/'}));
 
 env.config();
 app.use(express.json());
